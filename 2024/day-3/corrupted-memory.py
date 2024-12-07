b2e9de03aea66c50895d915e6e8525404c6ccaa2
Bearaffairs <itsabearaffair@gmail.com>
2024-12-07 16:10:40 +1030

    Smarter multiplier - 2024 day 3 part 2
    	- Include regex for "do()" & "don't()"

--- 2024/day-3/corrupted-memory.py
+++ 2024/day-3/corrupted-memory.py
@@ -5,18 +5,24 @@ def read_line(file):
     for line in open(file):
         yield line
 
-def part_one(file):
+def solve_puzzle(file):
     sum = 0
+    smarter_sum = 0
+    enabled = True
 
-    regex = re.compile(r'mul\(([0-9]{1,3}),([0-9]{1,3})\)')
+    regex = re.compile(r'mul\(([0-9]+),([0-9]+)\)|(don\'t\(\))|(do\(\))')
     for line in read_line(file):
-
         matches = regex.findall(line)
-        for x, y in matches:
-            sum += int(x)*int(y)
-
+        for x, y, dont, do in matches:
+            if do or dont:
+                enabled = bool(do)
+            if x and y:
+                val = int(x) * int(y)
+                sum += val
+                smarter_sum += val * enabled
 
     print(f'Part one sum is: {sum}')
+    print(f'Part two sum is: {smarter_sum}')
 
 
 if __name__ == '__main__':
@@ -25,4 +31,4 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--filename', type=str)
 
     args = parser.parse_args()
-    part_one(args.filename)
+    solve_puzzle(args.filename)
